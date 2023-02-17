51c1a766866fdf10a82646f7efc817f6c38234c5
Samuel Palmér <samiskramis@gmail.com>
2023-02-17 15:42:35 +0100

    IP Connection done

--- BattleShipsClient/Engine.cs
+++ BattleShipsClient/Engine.cs
@@ -7,10 +7,22 @@ public class Engine
     private string ipAddress;
     private readonly string port = ":3000";
 
+    public int lobbyID;
+
+    enum Action
+    {
+        Menu,
+        Playing,
+        Help
+    }
+
     public Engine()
     {
         string httpStart = "http://";
         string enteredAddress = "";
+        bool failed = false;
+
+    RedoAddress:
 
         while (true)
         {
@@ -18,6 +30,11 @@ public class Engine
             Raylib.ClearBackground(Color.WHITE);
 
             Raylib.DrawText("Current IP endpoint:\n" + httpStart + enteredAddress + "\n(this app uses port 3000)", 100, 100, 32, Color.RED);
+
+            if (failed)
+            {
+                Raylib.DrawText("Error on IP: " + ipAddress, 100, 500, 32, Color.RED);
+            }
 
             Raylib.EndDrawing();
 
@@ -36,7 +53,27 @@ public class Engine
 
         ipAddress = httpStart + enteredAddress + port;
 
-        Console.WriteLine(ipAddress);
+        Console.WriteLine("Using: " + ipAddress);
+
+        client = new(ipAddress);
+
+        RestRequest tryIP = new("/TestConnection", Method.Get);
+        try
+        {
+            RestResponse result = client.Get(tryIP);
+            if (result.IsSuccessful) return;
+            else
+            {
+                failed = true;
+                goto RedoAddress;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Failed the call with exception: " + e);
+            failed = true;
+            goto RedoAddress;
+        }
     }
 
     public void Run()
